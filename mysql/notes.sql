2d224bcb5abd2a1a2a8e2eb10cfaa7335adb82dc
OggyMishra <ms.sourabh@outlook.com>
2021-02-28 21:18:17 +0530

    more sql command

--- mysql/notes.sql
+++ mysql/notes.sql
@@ -11,3 +11,17 @@ DELETE FROM customers
 WHERE twitter_handle IS NULL
 LIMIT 5;
 
+!-- When searching for a pattern containing the specific characters % or _, we can utilize the escape character \, similarly to its use in Python.
+SELECT * FROM books WHERE title LIKE '% 100\%';
+
+!-- What is the max chars for each type of data in sql ??
+!-- e.g Numeric: bit, tinyint, smallint, int, bigint, decimal, numeric, float, real
+!-- Date/Time: Date, Time, Datetime, Timestamp and year
+!-- Character String: Char, Varchar, Varchar(Max), Text.
+!-- Unicode Character/String : Nchar, NVarchar, NVarchar(max), NText
+!-- Binary: Binary, Varbinary, Varbinary(max), image
+!-- Miscellaneous: Clob, Blob, XML, JSON
+
+
+
+

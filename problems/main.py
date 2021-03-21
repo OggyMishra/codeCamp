19d547d202ad0adccb74cdedfef4a8d9573ec62b
OggyMishra <ms.sourabh@outlook.com>
2021-03-21 21:59:54 +0530

    Adding breaking the palindrome problem

--- problems/main.py
+++ problems/main.py
@@ -219,5 +219,31 @@ def encryption(s):
 
 #endregion
 
+# Breaking the palindrome:
+#
+# Given a palindromic string of lowercase English letters palindrome, replace exactly one character with any lowercase English letter so that the resulting string is not a palindrome and that it is the lexicographically smallest one possible.
+#
+# Return the resulting string. If there is no way to replace a character to make it not a palindrome, return an empty string.
+#
+# A string a is lexicographically smaller than a string b (of the same length) if in the first position where a and b differ, a has a character strictly smaller than the corresponding character in b. For example, "abcc" is lexicographically smaller than "abcd" because the first position they differ is at the fourth character, and 'c' is smaller than 'd'.
+
+# Input: palindrome = "abccba"
+# Output: "aaccba"
+# Explanation: There are many ways to make "abccba" not a palindrome, such as "zbccba", "aaccba", and "abacba".
+# Of all the ways, "aaccba" is the lexicographically smallest.
+def breakPalindrome(input):
+    input_len = int(len(input))
+
+    if input_len <= 1:
+        return ''
+
+    for idx in range(0, int(input_len/2)):
+        if input[idx] != 'a':
+            return input[0: idx] + 'a' + input[idx+1:]
+    return input[0: input_len-1] + 'b'
+
+
+#endregion
+
 if __name__ == '__main__':
-    print(encryption('chillout'))
+    print(breakPalindrome('madam'))

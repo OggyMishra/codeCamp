f4be324ddee276383700cc2e1bb7439a44785a1f
OggyMishra <ms.sourabh@outlook.com>
2021-05-15 23:35:00 +0530

    Added two sorted merge problem

--- problems/main.py
+++ problems/main.py
@@ -646,7 +646,25 @@ def duplicateZeros(arr: List[int]) -> None:
 
 # endregion 
 
+
+# region 
+# Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as one sorted array.
+# The number of elements initialized in nums1 and nums2 are m and n respectively. You may assume that nums1 has a size equal to m + n such that it has enough space to hold additional elements from nums2.
+# e.g1: Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+# Output: [1,2,2,3,5,6]
+
+# e.g2: Input: nums1 = [1], m = 1, nums2 = [], n = 0
+# Output: [1]
+
+def merge(nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+    nums1[:] = nums1[:m] + nums2[:n]
+    nums1.sort(reverse=False)
+    print(nums1)
+        
+# endregion
 if __name__ == '__main__':
-    input =  [1,0,2,3,0,4,5,0]
-    res = duplicateZeros(input)
-    print(res)
+    nums1 = [1,2,3,0,0,0]
+    m = 3
+    n = 3
+    nums2 = [2,5,6]
+    merge(nums1, m, nums2, n)
